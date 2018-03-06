5e8bb341316ce0769c375c7bcdc14a219f0fbbeb
lilit <lilit.melkumyan1000@mail.ru>
2018-03-07 01:45:26 +0400

    Added lesson1

---
+++ lesson1/Lilit/Lessons/lesson1/scripts/script1.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cd ~/Desktop/Lessons/lesson1/output
+mkdir u1
+cd u1
+mkdir u2 
+touch a.txt
+cd u2
+touch a.txt b.txt
+
---
+++ lesson1/Lilit/Lessons/lesson1/scripts/script2.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+cd Desktop/Lessons/lesson1/u1
+value= 'cat a.txt'
+echo "$value"
+
