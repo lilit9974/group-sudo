11860095692d1574d629782b8730bc4de8ee4c38
Lilit <lilit.melkumyan1000@mail.ru>
2018-04-18 23:53:27 +0400

    homework

--- lesson4/pointer.cpp
+++ lesson4/pointer.cpp
@@ -4,23 +4,30 @@ class Point {
 		Point()  
 		{}
 		~Point() {}
-        void print() {
+        void foo() {
             std::cout << "Point" << std::endl;
         }
+	virtual void boo() {
+	    std::cout << "V Point" << std::endl;
+	}
 };
 class Rectangle: public Point {
 	public:
 		Rectangle()
 			{}
 		~Rectangle() {}
-        void print() {
+        void foo() {
             std::cout << "Rectangle" << std::endl;
         }
+	virtual void boo() {
+	    std::cout << "V Rect" << std::endl;
+	}
 };
 
 int main() {
     Rectangle R;
     Point* a = &R;
-    a->print();
+    a->foo();
+    a->boo();
 	return 0;
 }
